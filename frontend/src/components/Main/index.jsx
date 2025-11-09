9f8c100f29f0c276b017a52fe1560018e0b28235
Ivan <ipacucka@gmail.com>
2025-11-09 23:40:34 +0300

    Разметка

---
+++ frontend/src/components/Main/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <div className="main-inf">
+      <div className="main-container">
+        <h1>Основная фигня</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,9 +1,19 @@
-function Home(){
-    return(
-        <>
-        Вы вошли
-        </>
-    )
-}
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import SideBar from '../../components/SideBar';
+import Main from '../../components/Main';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <SideBar />
+        <Main />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home
